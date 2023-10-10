0d37ab4168114cdbe658660c037aeb8178d6a8da
Gebremedhin <kahsay.gebremedhin@powercoders.org>
2023-10-10 13:36:21 +0200

    Updated even or odd number checker

---
+++ Week_4/intro_JS/odd_even_reporter2.js
@@ -0,0 +1,11 @@
+//Write a for loop that will iterate from 0 to 20.
+//For each iteration, it will check if the current number is even or odd.
+//Report the result to the screen (e.g. "2 is even").
+
+for (let num = 0; num <= 20; num++){
+    if (num % 2 === 0) {
+        console.log(num + " is even");
+      } else {
+        console.log(num + " is odd");
+      }
+} 
