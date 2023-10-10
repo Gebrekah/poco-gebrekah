087c27e5c9330b5ffc995fae133f5e8a328d1d66
Gebremedhin <kahsay.gebremedhin@powercoders.org>
2023-10-10 09:55:54 +0200

    Age calculator using JS

---
+++ Week_4/intro_JS/age_calculator.js
@@ -0,0 +1,6 @@
+//Age calculator
+
+let birth_year = prompt();
+let future_year = prompt();
+let age = future_year - birth_year; 
+alert("I will be either " + age + " or " + (age-1) + " in " + future_year);
